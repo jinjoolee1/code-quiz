1838ae4b2d0bcc6d9fcc77fb7242a382ca019433
jinjoolee1 <jinjoolee126@gmail.com>
2022-04-03 12:23:15 -0500

    questions fixed

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,26 +1,36 @@
 //Questions
 const questions = [{
-    question: "Which of the following is not a commonly used data type?",
+    question: "Which of the following is NOT a commonly used data type?",
     answers: [ "Alerts", "Strings", "Booleans", "Numbers"],
     correctAnswer: "Alerts"
 
 }, {
-    question: "What can arrays in JavaScript can be used to store?",
-    answers: [ "Booleans",  "Numbers and strings", "Other arrays", "All of the above"], 
+    question: "The condition in an 'If/Else' statement must be contained in",
+    answers: [ "Brackets",  "Curly braces", "Parenthesis", "Quotations"], 
+    correctAnswer: "Parenthesis"
+    
+}, {
+    question: "String values must be enclosed within ____ when being assigned to variables",
+    answers: ["Quotes",  "Square brackets",  "Curly braces",  "Parentheses"],
+    correctAnswer: "Curly braces" 
+    
+}, {
+    question: "Arrays in Javascript can be used to store",
+    answers: ["Other arrays", "Numbers and strings",  "Booleans",  "All of the above"], 
     correctAnswer: "All of the above"
-    
-}, {
-    question: "How do you enclose the condition of an ' if ' statement?",
-    answers: ["Quotes",  "Square brackets",  "Curly brackets",  "Parentheses"],
-    correctAnswer: "Parentheses" 
-    
-}, {
-    question: "What is a very useful tool for debugging and printing content to the debugger?",
-    answers: ["Console.log", "For loops",  "CSS",  "Terminal/Bash"], 
-    correctAnswer: "Console.log"
 
 }, {
-    question: "What does DOM stand for?",
-    answers: ["Do Over Mulligan", "Document Object Model", "Data Object Model",  "Document Option Model"], 
-    correctAnswer: "Document Object Model"
+    question: "A very useful tool used during development and debugging for printing content to the debugger is",
+    answers: ["Javascript", "for loops", "console.log",  "terminal/bash"], 
+    correctAnswer: "terminal/bash"
 }];
+
+const timerEl = document.getElementById("timer");
+//buttons
+const startButton = document.getElementById('start-btn')
+const answerButtonsElement = document.getElementById('answer-buttons')
+const submitButton = document.getElementById('submit-btn')
+const answer1 = document.getElementById("btn1");
+const answer2 = document.getElementById("btn2");
+const answer3 = document.getElementById("btn3");
+const answer4 = document.getElementById("btn4");
