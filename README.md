bdcd625974323a3678cbfe32e5ef519184066eda
jinjoolee1 <jinjoolee126@gmail.com>
2022-04-03 11:58:43 -0500

    Questions added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# code-quiz
+# Code-quiz
+
+A timed (75 seoconds) coding quiz with multiple choice questions. 5 questions in total. Each correct answer adds 10 seconds to the timer, each incorrect answer subtracts 10 seconds from the timer. 
+
+Made by EC
---
+++ assets/Javascript/script.js
@@ -0,0 +1,26 @@
+//Questions
+const questions = [{
+    question: "Which of the following is not a commonly used data type?",
+    answers: [ "Alerts", "Strings", "Booleans", "Numbers"],
+    correctAnswer: "Alerts"
+
+}, {
+    question: "What can arrays in JavaScript can be used to store?",
+    answers: [ "Booleans",  "Numbers and strings", "Other arrays", "All of the above"], 
+    correctAnswer: "All of the above"
+    
+}, {
+    question: "How do you enclose the condition of an ' if ' statement?",
+    answers: ["Quotes",  "Square brackets",  "Curly brackets",  "Parentheses"],
+    correctAnswer: "Parentheses" 
+    
+}, {
+    question: "What is a very useful tool for debugging and printing content to the debugger?",
+    answers: ["Console.log", "For loops",  "CSS",  "Terminal/Bash"], 
+    correctAnswer: "Console.log"
+
+}, {
+    question: "What does DOM stand for?",
+    answers: ["Do Over Mulligan", "Document Object Model", "Data Object Model",  "Document Option Model"], 
+    correctAnswer: "Document Object Model"
+}];
